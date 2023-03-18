c706642cbf254beb79ba94094360024fd19378c4
Vitlyr <103600474+Vitlyr@users.noreply.github.com>
2023-03-17 22:06:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
-# This example requires the 'message_content' privileged intents
-
 import os
-import discord
-from discord.ext import commands
+from disnake.ext import commands
+import disnake
+import asyncio
 
+intents = disnake.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix=commands.when_mentioned, intents=intents)
 
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
-
+GUILD_ID = 1043647302272827412
+LOG_CHANNEL_ID = 1050550574652858429
+async def get_log_channel(guild):
+    return guild.get_channel(LOG_CHANNEL_ID)
 
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user}")
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})\n------")
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+@bot.slash_command(name="help", description="Displays a list of available commands.")
+async def help(ctx):
+    embed = disnake.Embed(title="Available Commands", color=0xD3D3D3)
+    for command in bot.slash_commands:
+        embed.add_field(name=f"/{command.name}", value=command.description, inline=False)
+    await ctx.send(embed=embed)
+    guild = bot.get_guild(GUILD_ID)
+    log_channel = await get_log_channel(guild)
+    embed = disnake.Embed(title="Command Executed", color=0xD3D3D3)
+    embed.add_field(name="User", value=f"{ctx.author.name}#{ctx.author.discriminator} (<@{ctx.user.id}>)")
+    embed.add_field(name="Command", value="/help")
+    
+    await log_channel.send(embed=embed)
 
-@bot.command()
-async def hello(ctx):
-    await ctx.send("Choo choo! 🚅")
-
-
-bot.run(os.environ["DISCORD_TOKEN"])
+if __name__ == "__main__":
+    bot.run(os.getenv("DISCORD_TOKEN"))
